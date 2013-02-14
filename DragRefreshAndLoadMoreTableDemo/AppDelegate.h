2abf468e1cc8541b471db52c8a4e27a5dadb07e4
openthread <openfibers@gmail.com>
2013-02-15 00:38:54 +0800

    Initial Commit

---
+++ DragRefreshAndLoadMoreTableDemo/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  DragRefreshAndLoadMoreTableDemo
+//
+//  Created by openthread on 2/15/13.
+//  Copyright (c) 2013 openthread. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ DragRefreshAndLoadMoreTableDemo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  DragRefreshAndLoadMoreTableDemo
+//
+//  Created by openthread on 2/15/13.
+//  Copyright (c) 2013 openthread. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
